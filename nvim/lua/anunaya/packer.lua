f39af32afacff543ba3037f8a8a8ca8f2f11c9e8
Anunaya Srivastava <anunaya.social@gmail.com>
2024-11-06 19:02:12 +0530

    Auto install packer

--- nvim/lua/anunaya/packer.lua
+++ nvim/lua/anunaya/packer.lua
@@ -1,7 +1,20 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
@@ -163,5 +176,9 @@ return require('packer').startup(function(use)
     "folke/todo-comments.nvim",
     requires = { "nvim-lua/plenary.nvim" },
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 
 end)
